8b3d33932e89e43d22321b98873386d28bc1a63c
VitorAlan <VitorAlan.bs@gmail.com>
2019-10-23 11:12:38 -0300

    Ajuste nas telas de Usuário e commit em pastas do projeto que não estavam presentes anteriormente

---
+++ test/controllers/motorista_controller_test.rb
@@ -0,0 +1,48 @@
+require 'test_helper'
+
+class MotoristaControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @motorista = motorista(:one)
+  end
+
+  test "should get index" do
+    get motorista_index_url
+    assert_response :success
+  end
+
+  test "should get new" do
+    get new_motorista_url
+    assert_response :success
+  end
+
+  test "should create motorista" do
+    assert_difference('Motorista.count') do
+      post motorista_index_url, params: { motorista: { bancoDeHoras: @motorista.bancoDeHoras, cpf: @motorista.cpf, nome: @motorista.nome, numeroDeHoras: @motorista.numeroDeHoras, telefone: @motorista.telefone } }
+    end
+
+    assert_redirected_to motorista_url(Motorista.last)
+  end
+
+  test "should show motorista" do
+    get motorista_url(@motorista)
+    assert_response :success
+  end
+
+  test "should get edit" do
+    get edit_motorista_url(@motorista)
+    assert_response :success
+  end
+
+  test "should update motorista" do
+    patch motorista_url(@motorista), params: { motorista: { bancoDeHoras: @motorista.bancoDeHoras, cpf: @motorista.cpf, nome: @motorista.nome, numeroDeHoras: @motorista.numeroDeHoras, telefone: @motorista.telefone } }
+    assert_redirected_to motorista_url(@motorista)
+  end
+
+  test "should destroy motorista" do
+    assert_difference('Motorista.count', -1) do
+      delete motorista_url(@motorista)
+    end
+
+    assert_redirected_to motorista_index_url
+  end
+end
---
+++ test/system/motorista_test.rb
@@ -0,0 +1,9 @@
+require "application_system_test_case"
+
+class MotoristaTest < ApplicationSystemTestCase
+  # test "visiting the index" do
+  #   visit motorista_url
+  #
+  #   assert_selector "h1", text: "Motorista"
+  # end
+end
