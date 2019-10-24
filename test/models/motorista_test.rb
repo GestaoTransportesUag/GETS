f9b649414290ee2d1fe100de1d38ae6729aaabba
VitorAlan <VitorAlan.bs@gmail.com>
2019-10-24 00:16:11 -0300

    Testes de Model: Motorista

--- test/models/motorista_test.rb
+++ test/models/motorista_test.rb
@@ -4,4 +4,20 @@ class MotoristaTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  test 'Nao deve salvar um motorista sem cpf' do
+    motorista = Motorista.new
+    assert_not motorista.save
+  end
+
+  test 'Nao deve salvar um motorista sem nome' do
+    motorista = Motorista.new cpf:11111111111, numeroDeHoras: 0, bancoDeHoras: false, telefone: 981380507
+    assert_not motorista.save
+  end
+
+  test 'Nao deve salvar um motorista com cpf menor que 11 digitos' do
+    motorista = Motorista.new cpf: 1111111111,nome: "Jose", numeroDeHoras: 0, bancoDeHoras: false, telefone: 981380507
+    assert_not motorista.save
+  end
+
+
 end
