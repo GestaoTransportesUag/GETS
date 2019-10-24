67fe465060d4f3b59ab3c8bf061dcad2593ff4f4
VitorAlan <VitorAlan.bs@gmail.com>
2019-10-24 00:33:08 -0300

    Testes de Model: Usuarios

--- test/models/usuario_test.rb
+++ test/models/usuario_test.rb
@@ -4,4 +4,18 @@ class UsuarioTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  test 'Nao deve salvar um usuario sem cpf' do
+    usuario = Usuario.new
+    assert_not usuario.save
+  end
+
+  test 'Nao deve salvar um usuario sem email' do
+    usuario = Usuario.new cpf:11111111111, nome: "Jose", senha: 000000,  telefone: 981380507, capacidadeDeGerencia: false
+    assert_not usuario.save
+  end
+
+  test 'Nao deve salvar um usuario com senha maior que 6 digitos' do
+    usuario = Usuario.new cpf: 1111111111, nome: "Jose", senha: 0000000000, email: "qualquercoisa@gmail.com", telefone: 981380507, capacidadeDeGerencia: false
+    assert_not usuario.save
+  end
 end
